0f7e72fb0d2cd642f6a34d7a2387d9ef6a48a7d4
eayumi <72615755+eayumi@users.noreply.github.com>
2021-05-31 14:24:05 +0200

    .value was still wrong

--- js/identify_transitions.js
+++ js/identify_transitions.js
@@ -288,9 +288,13 @@ function histo_trans_time(data, color, name) {
     var tot_double = 0;
     
     for(var j = 0;j <data.length; j++){
-        console.log(data[j].values[0])
-        tot_single+= data[j].values[0].value
-        tot_double+= data[j].values[1].value
+        console.log(data[j].values[0]);
+        tot_single+= data[j].values[0].value;
+        tot_double+= data[j].values[1].value;
+                console.log(data[j].values[1]);
+                        console.log(data[j].values[1].value);
+
+
         
     }
     tot = tot_single+tot_double;
